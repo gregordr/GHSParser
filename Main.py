7c2caa16baf996405cf8646907441cdaa47306b8
Somethingweirdhere <greetechprog@gmail.com>
2019-03-06 17:06:09 +0100

    Main

--- Main.py
+++ Main.py
@@ -1,1 +1,7 @@
 import Dangers
+
+def getForAll(compounds):
+    dangers = []
+    for compound in compounds:
+        dangers.append([compound] + Dangers.getAllDangers(compound))
+    return dangers
